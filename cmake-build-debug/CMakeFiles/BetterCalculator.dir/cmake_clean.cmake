51820589585390087f68264e5d6f5b25846b1c4b
HelsinkiJava <gmaki@live.com>
2021-10-10 02:18:43 -0700

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/BetterCalculator.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BetterCalculator.exe"
+  "BetterCalculator.exe.manifest"
+  "BetterCalculator.pdb"
+  "CMakeFiles/BetterCalculator.dir/main.c.obj"
+  "libBetterCalculator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BetterCalculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
